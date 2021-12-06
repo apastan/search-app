ffa386a8f5ff7d326e804aa19b91803339db3945
apastan <zubovst@gmail.com>
2021-12-06 13:24:44 +0300

    Create Header Component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,116 @@
+import React, {FC} from 'react';
+import {NavLink} from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+
+export const Header: FC = () => {
+    console.log(`Рендер Header Component`);
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const pages =
+        [
+            {name: 'search-1', to: '/search-client-autocomplete'},
+            {name: 'search-2', to: '/search-client-onclick'},
+            {name: 'search-3', to: '/search-server-autocomplete'},
+            {name: 'search-4', to: '/search-server-onclick'},
+        ]/*.map((page) => ({...page, isActive: pathname === page.to ? true : false}));*/
+
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="lg">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{mr: 2, display: {xs: 'none', md: 'flex'}}}
+                    >
+                        LOGO
+                    </Typography>
+
+
+                    <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: {xs: 'block', md: 'none'},
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem
+                                    key={page.name}
+                                    onClick={handleCloseNavMenu}
+                                    component={NavLink}
+                                    //variant={page.isActive ? "outlined" : "text"}
+                                    to={page.to}>
+                                    <Typography textAlign="center">{page.name}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}
+                    >
+                        LOGO
+                    </Typography>
+                    <Box sx={{flexGrow: 1, ml: 6, display: {xs: 'none', md: 'flex'}}}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page.name}
+                                onClick={handleCloseNavMenu}
+                                component={NavLink}
+                                to={page.to}
+                                sx={{my: 2, color: 'white', display: 'block', ml: 4}}
+                            >
+                                {page.name}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
--- src/components/layouts/BaseLayout.tsx
+++ src/components/layouts/BaseLayout.tsx
@@ -1,9 +1,11 @@
 import React, {FC} from 'react';
 import { Outlet } from "react-router-dom";
+import {Header} from '../Header/Header';
 
 export const BaseLayout: FC = () => {
     return (
         <>
+            <Header/>
             <Outlet/>
         </>
     );
